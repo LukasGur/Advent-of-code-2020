407b9bca676e3e251be1203aa57065b63055f5e2
Lukáš Gurecký <l.gurecky@seznam.cz>
2020-12-09 10:33:44 +0100

    finish: day 6

--- 6.den/index.js
+++ 6.den/index.js
@@ -1,5 +1,22 @@
 const readData = require('../readFromFile');
 
 readData((response) => {
-    console.log(response);
+    response = response.split('\n\n');
+    let answer = 0;
+    response.forEach(group => {
+        let arrayOfQuestions = [];
+        group.split('\n').forEach((person, indexOfPerson) => {
+            person = person.split('');
+            
+            if (indexOfPerson === 0) {
+                return arrayOfQuestions = person;
+            }
+
+            arrayOfQuestions = person.filter(x => arrayOfQuestions.includes(x));
+        });
+
+        answer += arrayOfQuestions.length;
+    });
+
+    console.log(answer);
 });
